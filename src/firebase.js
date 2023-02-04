0e62c790cb7fb996b8d0d1efc90c1032c0a29ca8
dripminister <95714820+dripminister@users.noreply.github.com>
2023-02-04 15:29:47 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDQCxD2iHgLWyxcsatezbUAR6xOh6g3Law",
-	authDomain: "realtor-clone-90e20.firebaseapp.com",
-	projectId: "realtor-clone-90e20",
-	storageBucket: "realtor-clone-90e20.appspot.com",
-	messagingSenderId: "878784693565",
-	appId: "1:878784693565:web:13e9c633505b92d9c37154",
+//Firebase Config
 }
 
 const app = initializeApp(firebaseConfig)
